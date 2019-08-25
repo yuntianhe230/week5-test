764f52879004a92cd9c362b6b83aee4230c47a54
yuntianhe230 <54255068+yuntianhe230@users.noreply.github.com>
2019-08-25 17:54:23 +1000

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static('views'));
+let db = [];
+db.push({
+    carId: 0,
+    carMake: 'BMW',
+    carModel: '735',
+    carYear: 2014
+});
+db.push({
+    carId: 1,
+    carMake: 'Mercedes',
+    carModel: 'C250',
+    carYear: 2017
+});
+db.push({
+    carId: 3,
+    carMake: 'Audi',
+    carModel: 'A6',
+    carYear: 2019
+});
+app.get('/', function (req, res) {
+    res.render('thankyou.html', {username: "Guest", carDb: db});
+});
+app.listen(8085);
